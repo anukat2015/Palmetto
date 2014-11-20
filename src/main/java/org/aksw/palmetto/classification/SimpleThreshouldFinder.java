1d33ff1c857bdb6d8d8f9d5356579489e90de8c9
MichaelRoeder <michaderstudent@hotmail.de>
2014-11-20 12:38:53 +0100

    Implemented the functionality of the simple threshould finder.

--- src/main/java/org/aksw/palmetto/classification/SimpleThreshouldFinder.java
+++ src/main/java/org/aksw/palmetto/classification/SimpleThreshouldFinder.java
@@ -3,102 +3,121 @@ package org.aksw.palmetto.classification;
 import java.io.PrintStream;
 import java.util.Arrays;
 
-import org.apache.commons.io.IOUtils;
-
 import com.carrotsearch.hppc.BitSet;
-import com.carrotsearch.hppc.LongOpenHashSet;
+import com.carrotsearch.hppc.DoubleObjectOpenHashMap;
 
 public class SimpleThreshouldFinder {
 
+    private static final int TRUE_POSITIVE_INDEX = 0;
+    private static final int FALSE_POSITIVE_INDEX = 1;
+    private static final int PRECISION_INDEX = 0;
+    private static final int RECALL_INDEX = 0;
+    private static final int F1_INDEX = 0;
+
+    public double findThreshould(double goldStd[], double data[], double humanRatingThreshould, PrintStream debugStream) {
+        BitSet isTruePositive = new BitSet(goldStd.length);
+        for (int i = 0; i < goldStd.length; i++) {
+            if (goldStd[i] >= humanRatingThreshould) {
+                isTruePositive.set(i);
+            }
+        }
+        return findThreshould(data, isTruePositive, debugStream);
+    }
+
     public double findThreshould(double data[], BitSet isTruePositive, PrintStream debugStream) {
-        // go through the data and create a mapping double --> number of positive, negatives which are not classified as
+        // go through the data and create a mapping double --> number of
+        // positive, negatives which are not classified as
         // good anymore
+        DoubleObjectOpenHashMap<int[]> mapping = createMapping(data, isTruePositive);
         // create a sorted array of values
-        // count the true positives (= all that are assigned to the ) and false positives
-        // go through the mapping according to the sorted array (make sure that the key is the threshould, but a
-        // threshould is used for value >= threshould, thus, we have to deal with threshoulds pointing to values that
-        // should be used of the threshould is larger than this number) and decrease the number of true positives and
+        double values[] = mapping.keys().toArray();
+        Arrays.sort(values);
+        // count the true positives (= all that are assigned to the ) and false
+        // positives
+        int positivesInGoldStd = (int) isTruePositive.cardinality();
+        int truePositives = positivesInGoldStd;
+        int falsePositives = data.length - positivesInGoldStd;
+        // go through the mapping according to the sorted array (make sure that
+        // the key is the threshould, but a
+        // threshould is used for value >= threshould, thus, we have to deal
+        // with threshoulds pointing to values that
+        // should be used of the threshould is larger than this number) and
+        // decrease the number of true positives and
         // false positives according to the numbers inside
         // the mapping.
-        // For every step update precision, recall and F1
-        return 0;
-    }
-
-    private double[] findBestThreshould(LongOpenHashSet goldStandard, double[][] similarities, double[] similaritySteps) {
-        PrintStream resultOut = null;
-        try {
-            resultOut = new PrintStream("");
-            resultOut.println("Threshould,Precision,Recall,F1-measure");
-
-            Arrays.sort(similaritySteps);
-            double result[], bestResult[] = new double[] { 0, 0, 0 };
-
-            result = calculateF1(goldStandard, similarities, similaritySteps[0]);
-            resultOut.print("0,");
-            resultOut.print(result[0]);
-            resultOut.print(',');
-            resultOut.print(result[1]);
-            resultOut.print(',');
-            resultOut.println(result[2]);
-            resultOut.print(similaritySteps[0]);
-            resultOut.print(',');
-            resultOut.print(result[0]);
-            resultOut.print(',');
-            resultOut.print(result[1]);
-            resultOut.print(',');
-            resultOut.println(result[2]);
-
-            for (int i = 1; i < similaritySteps.length; ++i) {
-                result = calculateF1(goldStandard, similarities, similaritySteps[i]);
-                if (result[2] > bestResult[2]) {
-                    bestResult = result;
-                }
-                resultOut.print(similaritySteps[i]);
-                resultOut.print(',');
-                resultOut.print(result[0]);
-                resultOut.print(',');
-                resultOut.print(result[1]);
-                resultOut.print(',');
-                resultOut.println(result[2]);
-            }
-            if (similaritySteps[similaritySteps.length - 1] < 1.0) {
-                resultOut.println("1,0,0");
-            }
-            return bestResult;
-        } catch (Exception e) {
-            LOGGER.error("Error while printing results to file. Returning null.", e);
-            return null;
-        } finally {
-            IOUtils.closeQuietly(resultOut);
+        double result[] = calculateF1(truePositives, falsePositives, positivesInGoldStd);
+        int counts[];
+        double bestF1 = result[F1_INDEX];
+        int bestThreshouldId = 0;
+        if (debugStream != null) {
+            debugStream.println("threshould\ttruePositives\tfalsePositives\tprecision\trecall\tf1");
+            debugStream.print(values[0]);
+            debugStream.print('\t');
+            debugStream.print(truePositives);
+            debugStream.print('\t');
+            debugStream.print(falsePositives);
+            debugStream.print('\t');
+            debugStream.print(result[PRECISION_INDEX]);
+            debugStream.print('\t');
+            debugStream.print(result[RECALL_INDEX]);
+            debugStream.print('\t');
+            debugStream.println(result[F1_INDEX]);
         }
-    }
-
-    private double[] calculateF1(LongOpenHashSet goldStandard, double[][] similarities, double threshould) {
-        int truePos = 0, falsePos = 0;
-        long pair;
-        for (int i = 0; i < similarities.length; ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (similarities[i][j] >= threshould) {
-                    pair = (((long) j) << 32) | (long) i;
-                    if (goldStandard.contains(pair)) {
-                        ++truePos;
-                    } else {
-                        ++falsePos;
-                    }
-                }
+        for (int i = 0; i < (values.length - 1); i++) {
+            counts = mapping.get(values[i]);
+            truePositives -= counts[TRUE_POSITIVE_INDEX];
+            falsePositives -= counts[FALSE_POSITIVE_INDEX];
+            // For every step update precision, recall and F1
+            result = calculateF1(truePositives, falsePositives, positivesInGoldStd);
+            if (bestF1 < result[F1_INDEX]) {
+                bestF1 = result[F1_INDEX];
+                bestThreshouldId = i + 1;
+            }
+            if (debugStream != null) {
+                debugStream.print(values[i + 1]);
+                debugStream.print('\t');
+                debugStream.print(truePositives);
+                debugStream.print('\t');
+                debugStream.print(falsePositives);
+                debugStream.print('\t');
+                debugStream.print(result[PRECISION_INDEX]);
+                debugStream.print('\t');
+                debugStream.print(result[RECALL_INDEX]);
+                debugStream.print('\t');
+                debugStream.println(result[F1_INDEX]);
             }
         }
+        return values[bestThreshouldId];
+    }
+
+    private DoubleObjectOpenHashMap<int[]> createMapping(double[] data, BitSet isTruePositive) {
+        DoubleObjectOpenHashMap<int[]> mapping = new DoubleObjectOpenHashMap<int[]>();
+        int counts[];
+        for (int i = 0; i < data.length; i++) {
+            if (mapping.containsKey(data[i])) {
+                counts = mapping.lget();
+            } else {
+                counts = new int[] { 0, 0 };
+                mapping.put(data[i], counts);
+            }
+            ++counts[(isTruePositive.get(i) ? TRUE_POSITIVE_INDEX : FALSE_POSITIVE_INDEX)];
+        }
+        return mapping;
+    }
+
+    private double[] calculateF1(int truePos, int falsePos, int posInGoldStd) {
         double result[] = new double[3];
         // precision
         if ((truePos + falsePos) > 0) {
-            result[0] = ((double) truePos) / (double) (truePos + falsePos);
+            result[PRECISION_INDEX] = ((double) truePos) / (double) (truePos + falsePos);
         } else {
-            result[0] = 1;
+            result[PRECISION_INDEX] = 1;
         }
         // recall
-        result[1] = ((double) truePos) / ((double) goldStandard.size());
+        result[RECALL_INDEX] = ((double) truePos) / ((double) (posInGoldStd));
         // F1
-        result[2] = (2 * result[0] * result[1]) / (result[0] + result[1]);
+        result[F1_INDEX] = (2 * result[PRECISION_INDEX] * result[RECALL_INDEX])
+                / (result[PRECISION_INDEX] + result[RECALL_INDEX]);
         return result;
     }
 }
